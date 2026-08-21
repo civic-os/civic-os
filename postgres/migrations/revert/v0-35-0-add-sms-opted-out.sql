a79159dfc8010861c454421df11b59c0525ceb2e
Dan Kurin <dan@leapfinder.com>
2026-08-21 15:44:46 -0400

    Fix v0-35-0 revert: drop notification_preferences VIEW before dropping sms_opted_out column

--- postgres/migrations/revert/v0-35-0-add-sms-opted-out.sql
+++ postgres/migrations/revert/v0-35-0-add-sms-opted-out.sql
@@ -2,8 +2,15 @@
 
 BEGIN;
 
+-- Drop public VIEW that depends on the column we're about to remove
+DROP VIEW IF EXISTS public.notification_preferences;
+
 ALTER TABLE metadata.notification_preferences
     DROP COLUMN IF EXISTS sms_opted_out;
+
+-- Recreate public VIEW without the removed column
+CREATE OR REPLACE VIEW public.notification_preferences AS
+    SELECT * FROM metadata.notification_preferences;
 
 -- Restore original comment on phone_number column
 COMMENT ON COLUMN metadata.notification_preferences.phone_number IS
